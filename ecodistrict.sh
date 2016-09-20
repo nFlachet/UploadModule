e4fb3def802819e04e2d4d78ae29597555448d51
Baptiste Gaillet <baptiste.gaillet@cstb.fr>
2016-09-20 10:31:42 +0200

    Change ecodistrict script

--- ecodistrict.sh
+++ ecodistrict.sh
@@ -1,62 +1,36 @@
 #!/bin/sh
+# chkconfig: 123456 90 10
+# Tornado Server for ecodistrict uploader
+#
+workdir=/home/cstb/ecodistrict/FileHandlerService/
 
-### BEGIN INIT INFO
-# Provides:          myservice
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Put a short description of the service here
-# Description:       Put a long description of the service here
-### END INIT INFO
-
-# Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/home/cstb/ecodistrict/FileHandlerService/
-DAEMON=$DIR/UploadForm.py
-DAEMON_NAME=ecodisctrict_uploader
-
-# Add any command line options for your daemon here
-DAEMON_OPTS=""
-
-# This next line determines what user the script runs as.
-# Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
-DAEMON_USER=cstb
-
-# The process ID of the script when it runs is stored here:
-PIDFILE=/var/run/$DAEMON_NAME.pid
-
-. /lib/lsb/init-functions
-
-do_start () {
-    log_daemon_msg "Starting system $DAEMON_NAME daemon"
-    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON -- $DAEMON_OPTS
-    log_end_msg $?
+start() {
+    cd $workdir
+    /usr/bin/python /home/cstb/ecodistrict/FileHandlerService/UploadForm.py &
+    echo "Server started."
 }
-do_stop () {
-    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
-    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
-    log_end_msg $?
+
+stop() {
+    pid=`ps -ef | grep '[p]ython /home/cstb/ecodistrict/FileHandlerService/UploadForm.py' | awk '{ print $2 }'`
+    echo $pid
+    kill $pid
+    sleep 2
+    echo "Server killed."
 }
 
 case "$1" in
-
-    start|stop)
-        do_${1}
-        ;;
-
-    restart|reload|force-reload)
-        do_stop
-        do_start
-        ;;
-
-    status)
-        status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
-        ;;
-
-    *)
-        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
-        exit 1
-        ;;
-
+  start)
+    start
+    ;;
+  stop)
+    stop
+    ;;
+  restart)
+    stop
+    start
+    ;;
+  *)
+    echo "Usage: /etc/init.d/ecodistrict {start|stop|restart}"
+    exit 1
 esac
 exit 0
